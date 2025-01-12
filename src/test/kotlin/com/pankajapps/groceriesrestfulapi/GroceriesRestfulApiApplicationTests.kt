988bdd0ae4b4b9091595453b18b93e8d4bb6bccc
Pankaj Sachdeva <dearestpankaj@gmail.com>
2025-01-12 16:16:22 +0100

    Rix folder structure for Test files

---
+++ src/test/kotlin/com/pankajapps/groceriesrestfulapi/GroceriesRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pankajapps.groceriesrestfulapi.groceriesrestfulapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroceriesRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
